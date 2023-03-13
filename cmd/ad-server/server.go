be53c46c04b02ece80666bbcee3f534549f9f999
Kirill Kudryavcev <rtutiui@mail.ru>
2023-03-13 16:47:06 +0300

    add geolite2 city db

--- cmd/ad-server/server.go
+++ cmd/ad-server/server.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"github.com/oschwald/geoip2-golang"
 	"log"
 	"modules/internal/ad-server"
 )
 
 func main() {
-	s := adSrv.NewServer()
+	geoip, err := geoip2.Open("GeoLite2-City.mmdb")
+	if err != nil {
+		log.Fatalf("open geoip db ERROR: %s", err)
+	}
+
+	s := adSrv.NewServer(geoip)
 	if err := s.Listen(); err != nil {
 		log.Fatalf("Server listening ERROR: %s", err)
 	}
+
+	log.Print("server are listening: \n https://localhost:8080")
 }
--- internal/ad-server/server.go
+++ internal/ad-server/server.go
@@ -1,12 +1,21 @@
 package adSrv
 
-import "github.com/valyala/fasthttp"
+import (
+	"fmt"
+	realip "github.com/ferluci/fast-realip"
+	"github.com/mssola/user_agent"
+	"github.com/oschwald/geoip2-golang"
+	"github.com/valyala/fasthttp"
+	"log"
+	"net"
+)
 
 type Server struct {
+	geoip *geoip2.Reader
 }
 
-func NewServer() *Server {
-	return &Server{}
+func NewServer(geoip *geoip2.Reader) *Server {
+	return &Server{geoip: geoip}
 }
 
 func (s *Server) Listen() error {
@@ -14,5 +23,25 @@ func (s *Server) Listen() error {
 }
 
 func (s *Server) handleHttp(ctx *fasthttp.RequestCtx) {
-	ctx.WriteString("Hello consumer!")
+	ip := realip.FromRequest(ctx)
+	ua := string(ctx.Request.Header.UserAgent())
+	parsed := user_agent.New(ua)
+	browserName, browserVersion := parsed.Browser()
+
+	country, err := s.geoip.Country(net.ParseIP(ip))
+	if err != nil {
+		log.Fatalf("country parsing in geolite db ERROR: %s", err)
+	}
+
+	city, err := s.geoip.City(net.ParseIP(ip))
+	if err != nil {
+		log.Fatalf("city parsing in geolite db ERROR: %s", err)
+	}
+
+	ctx.WriteString(fmt.Sprintf("User-Agent: %s\n", ua))
+	ctx.WriteString(fmt.Sprintf("IP: %s\n", ip))
+	ctx.WriteString(fmt.Sprintf("Browser name: %s\n Browser verison: %s\n", browserName, browserVersion))
+	ctx.WriteString(fmt.Sprintf("Country: %s\n", country))
+	ctx.WriteString(fmt.Sprintf("City: %s\n", city))
+
 }
