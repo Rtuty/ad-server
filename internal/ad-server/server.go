a69e21ccd7ea822f6657d7f74e0a307bb2af699f
Kudryavtsev Kirill <rtutiui@mail.ru>
2023-03-15 00:14:52 +0300

    add new dao func

--- internal/ad-server/server.go
+++ internal/ad-server/server.go
@@ -9,16 +9,20 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
+	dao "modules/internal/db/mongo"
 	"net"
 )
 
 type Server struct {
 	geoip *geoip2.Reader
+	dao   *dao.CustomerMetric
 }
 
-func NewServer(geoip *geoip2.Reader) *Server {
-
-	return &Server{geoip: geoip}
+func NewServer(geoip *geoip2.Reader, dao *dao.CustomerMetric) *Server {
+	return &Server{
+		geoip: geoip,
+		dao:   dao,
+	}
 }
 
 func (s *Server) Listen() error {
@@ -26,6 +30,7 @@ func (s *Server) Listen() error {
 }
 
 func (s *Server) handleHttp(ctx *fasthttp.RequestCtx) {
+	// Создаем клиент MongoDB
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb:127.0.0.1:27017"))
 	if err != nil {
 		log.Fatalf("mongo db client ERROR: %s", err)
@@ -36,6 +41,7 @@ func (s *Server) handleHttp(ctx *fasthttp.RequestCtx) {
 		}
 	}()
 
+	// Собираем метрики, подключаемся к Geolite
 	ip := realip.FromRequest(ctx)
 	ua := string(ctx.Request.Header.UserAgent())
 	parsed := user_agent.New(ua)
@@ -56,4 +62,11 @@ func (s *Server) handleHttp(ctx *fasthttp.RequestCtx) {
 	ctx.WriteString(fmt.Sprintf("Browser name: %s Browser verison: %s\n", browserName, browserVersion))
 	ctx.WriteString(fmt.Sprintf("Country name: %s Code: %s\n", country.Country.Names, country.Country.IsoCode))
 	ctx.WriteString(fmt.Sprintf("City name: %s Code: %s\n", city.City.Names, city.City.GeoNameID))
+
+	//Создаем DAO структуру
+	CustomerMetric, err := dao.NewAdDAO(ctx, client)
+	if err != nil {
+		log.Fatalf("DAO client ERROR: %s", err)
+	}
+
 }
--- internal/db/mongo/mongo.go
+++ internal/db/mongo/mongo.go
@@ -1,7 +1,16 @@
 package mongo
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	"github.com/valyala/fasthttp"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
-type UrlDAO struct {
+type CustomerMetric struct {
 	c *mongo.Collection
+}
+
+func NewAdDAO(ctx *fasthttp.RequestCtx, client *mongo.Client) (*CustomerMetric, error) {
+	return &CustomerMetric{
+		c: client.Database("core").Collection("metrics"),
+	}, nil
 }
